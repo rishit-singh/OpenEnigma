60d02a21c863811fd17fa768318c8b6f01908ddf
Pardeep Kamboj <pkamboj@ARDANUY.local>
2020-06-30 10:57:54 +0530

    Directory Manager and Exception classes initialized,.

---
+++ include/exceptions.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <stdio.h>
+#include <iostream>
+#include <stack>
+#include <exception>
+
+namespace Exceptions
+{
+    class Exception
+    {
+    public:
+
+        char* Message;
+    };
+
+    class FileHandling
+    {
+        
+    };    
+}
--- include/filemanager.h
+++ include/filemanager.h
@@ -5,6 +5,32 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <stack>
+#include <ofstream> 
 
 namespace DirectoryManager
-{ };
+{
+    class Directory
+    {
+    public:
+        stack<Directory*>* NestedDirectories;
+
+        char* Name, *Root;
+
+        bool Add(Directory* directory) // Adds a directory.
+        {
+            this->NestedDirectories->Push(directory);
+        }
+
+        static bool IsValid(Directory* dir)
+        {
+            
+        }
+
+        Directory(char* name, Directory* root)
+        {
+            this->Name = name;
+            this->Root = root;
+        }
+    };
+}
